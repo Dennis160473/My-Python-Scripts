2745e5a1c2c4fcc6b9b7e72ed3bd6f9e4aa41cfd
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2024-12-11 13:45:16 +0100

    Update my_tests.py

---
+++ my_tests.py
@@ -0,0 +1,21 @@
+print ("Mein Name ist Dennis und ich übe die Programmierung mit Python")
+
+first_name="Dennis"
+last_name="Angermann"
+print(first_name+" "+last_name)
+
+zahl1 = 19
+zahl2 = 73
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
+print("Zahl 1 * Zahl 2:", zahl1 * zahl2)
+print("Zahl 1 / Zahl 2:", zahl1 / zahl2)
+
+print("16.04.1973")
+
+second_name="Test"
+third_name="bestanden"
+print(second_name+" "+third_name)
+
+first_name = input("Wie ist Dein Vorname: ")
+last_name = input("Wie ist Dein Nachname: ")
+print(first_name+" "+last_name)
