62e5558045961f426b5f424c4ac4ee9aebaac647
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2024-12-11 16:51:05 +0100

    Create Hausaufgabe11122024.py
    Erledigt!

---
+++ Hausaufgabe11122024.py
@@ -0,0 +1,46 @@
+### Aufgabe 2.1: Einfache Mathematik
+
+zahl1 = 4
+zahl2 = 2
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
+print("Zahl 1 - Zahl 2:", zahl1 - zahl2)
+print("Zahl 1 * Zahl 2:", zahl1 * zahl2)
+print("Zahl 1 / Zahl 2:", zahl1 / zahl2)
+
+
+
+### Aufgabe 2.2: Durchschnitt berechnen
+
+note1 = 2
+note2 = 3
+note3 = 4
+Zahlen = [2,3,4]
+average = sum(Zahlen)/len(Zahlen)
+print("Average of list: ", round(average,3))
+
+
+
+### Aufgabe 2.3 Kilometer in Meilen umrechnen: 1 Kilometer = 0.621371 Meilen
+
+
+print("Meilen <-> Kilometer")
+faktor = 0.621371
+
+print("(1) Meilen -> Kilometer")
+print("(2) Kilometer -> Meilen")
+print("(3) Beenden")
+auswahl = int(input("Deine Wahl: "))
+
+if auswahl == 1:
+    s = float(input("Strecke in Meilen: "))
+    s = s / faktor
+    print(f"Das sind {s}= Kilometer")
+elif auswahl == 2:
+    s = float(input("Strecke in Kilometern: "))
+    s = s * faktor
+    print(f"Das sind {s}= Meilen")
+else:
+    print("Diese Eingabe war nicht vorgesehen")
+
+print("Ende des Programms")
+
